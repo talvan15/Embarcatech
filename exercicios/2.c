9fe22b9f2cc2ec0ec3ad64b42902b8c5dfb38007
Talvan Dias <talvan.alves00@gmail.com>
2024-12-14 17:50:11 -0300

    First exercicio

---
+++ exercicios/2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int funcao(int x, int y){
+    return (x * y);
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Digite os dois numeros para multiplicar:\n");
+    scanf("%d %d", &x, &y);
+
+    printf("Resultado: %d", funcao(x, y));
+}
+
