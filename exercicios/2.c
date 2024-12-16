c2831b3ed0841776407f0c78bec59a763d62c209
Talvan Dias <talvan.alves00@gmail.com>
2024-12-16 10:32:56 -0300

    nova função para fatorial

--- exercicios/2.c
+++ exercicios/2.c
@@ -2,19 +2,36 @@
 #include <stdio.h>
 
 int funcao(int x, int y);
+int fatorial(int x);
 
 int main()
 {
-    int x, y;
+    int x, y, n;
 
-    printf("Digite os dois numeros para multiplicar:\n");
+    printf("\nDigite os dois numeros para multiplicar:\n");
     scanf("%d %d", &x, &y);
 
     printf("Resultado: %d", funcao(x, y));
+
+    printf("\nDigite um numero para calccular o fatorial: \n");
+    scanf("%d", &n);
+
+    printf("Fatorial: %d", fatorial(n));
 }
 
 int funcao(int x, int y){
     return (x * y);
+}
+
+int fatorial(int n){
+    int cont = 1;
+    if(n == 0 || n == 1) return 1;
+    else{
+        for(int i = 1; i <= n; i++){
+            cont *= i;
+        }
+        return cont;
+    }
 }
 
 
